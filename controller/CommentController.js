8bbce8fe5c1bc83107389294e9a913ee30c027c3
Guðni Ágústsson <gudnia10@ru.is>
2013-03-29 14:51:47 +0000

    Bætti við

---
+++ controller/CommentController.js
@@ -0,0 +1,8 @@
+Main.controller('AddCommentController', function ($scope, $location, $routeParams, NoteModel) {
+  var videoId = $routeParams.videoId;
+      $scope.createComment = function() {
+      NoteModel.addComment(videoId, $scope.comment.content);
+      $location.path('/video/' + videoId);
+    }
+  }
+);
