2a793d4957f59e01f6962ab3130aad5ebba4078c
Guðni Ágústsson <gudnia10@ru.is>
2013-04-03 19:45:57 +0000

    Breytti Teacher Controrlernum og viewinu

--- controller/TeacherController.js
+++ controller/TeacherController.js
@@ -2,4 +2,9 @@ Lect.controller ('TeacherController', function ($scope, $location, $routeParmas,
 	var lectures = LectureModel.getLectures();
 	$scope.lectures = lectures;
 
+	$scope.addLecture = function() {
+    $scope.lectures.push({text:$scope.lectureText, done:false});
+    $scope.lectureText = '';
+  };
+
 });
