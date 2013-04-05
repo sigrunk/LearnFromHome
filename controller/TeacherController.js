6e0c34fb07259be0af556b3bc0618a7e8cc9a2f2
Sigrun Inga Kristinsdottir <sigrunk08@ru.is>
2013-04-05 14:37:51 +0000

    er að vinna í að laga view þannig að comment sjáist ekki nema það sé valið

--- controller/TeacherController.js
+++ controller/TeacherController.js
@@ -1,4 +1,5 @@
 Lect.controller('TeacherController', function  ($scope, $location, $routeParams, LectureModel) {
 	var lectures = LectureModel.getLectures();
 	$scope.lectures = lectures;
+	$scope.selectedLectureId = $routeParams.lectureId;
 })
